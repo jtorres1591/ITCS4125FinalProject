fcdcdbbe6c709d6f21f32d2a0b7433c69c7ba833
Justin Torres <justintorres631@gmail.com>
2023-11-23 16:25:23 -0500

    Got Moving Camera to work.
    The moving camera is a bit janky though.

--- Assets/Scripts/AltCamera.cs
+++ Assets/Scripts/AltCamera.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class AltCamera : MonoBehaviour
 {
     public Rigidbody Rigid;
-    public float MouseSensitivity;
+    public float mouseSensitivity = 2.0f;
     public float MoveSpeed;
+
+    public Transform player;
+
+    float cameraVerticalRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +21,26 @@ public class AltCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(Input.GetAxis("Mouse Y") * -MouseSensitivity, Input.GetAxis("Mouse X") * MouseSensitivity, 0)));
+        // Movement
         Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
+        // Rotation
+
+        // Collect Mouse Input
+
+        float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        // Rotate the Camera around its local X axis
+
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+
+
+        // Rotate the Player Object and the Camera around its Y axis
+
+        player.Rotate(Vector3.up * inputX);
+
+
     }
 }
